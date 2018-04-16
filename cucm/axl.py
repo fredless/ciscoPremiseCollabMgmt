9f31adbc6cf6cad8b7777ec2cb5ccb7033dc1c5c
Fred Nielsen <16697069+freddyPlus@users.noreply.github.com>
2018-04-16 11:58:29 -0700

    add GPLv3 license header

--- cucm/axl.py
+++ cucm/axl.py
@@ -1,3 +1,20 @@
+# Copyright (C) 2018 Frederick w. Nielsen
+# 
+# This file is part of Cisco On-Premise Collab API Management Routines.
+# 
+# Cisco On-Premise Collab API Management Routines is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# Cisco On-Premise Collab API Management Routines is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with Cisco On-Premise Collab API Management Routines.  If not, see <http://www.gnu.org/licenses/>.
+
 import sys
 import dicttoxml
 import xmltodict
