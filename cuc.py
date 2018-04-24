2710353acf59f44e8cbda30452cbb919c7b55e57
Fred Nielsen <16697069+freddyPlus@users.noreply.github.com>
2018-04-24 15:51:33 -0700

    add sqlcsv method ssh class

--- cuc.py
+++ cuc.py
@@ -14,6 +14,9 @@
 # Cisco On-Premise Collab API Management Routines.  If not, see <http://www.gnu.org/licenses/>.
 
 import sys
+import re
+import csv
+import io
 
 # leverages SSH workers in VOS modules
 import api.vos
@@ -35,7 +38,47 @@ class ssh:
         self.cucvos = api.vos.ssh(userid, pw, nodeip)
         self.cucshell = self.cucvos.connect_interactive()
         
-    def sql (self, sql, db="unitydirdb"):
-        """ Execute CLI SQL query and return result as string. Common DB unitydirdb is default but can be overridden """
+    def sql (self, sql, db='unitydirdb'):
+        """ Execute CLI SQL query and return result. Common DB unitydirdb is default but can be overridden """
 
-        return(api.vos.ssh.send_command(self.cucvos, self.cucshell, f'run cuc dbquery {db} {sql}'),'\n')
+        return(api.vos.ssh.send_command(self.cucvos, self.cucshell, f'run cuc dbquery {db} {sql}'))
+
+    def sqlcsv(self, sql, db='unitydirdb'):
+        """ Execute CLI SQL query and return results as CSV.  Common DB unitydirdb is default but can be overridden """
+
+        result = api.vos.ssh.send_command(self.cucvos, self.cucshell, f'run cuc dbquery {db} {sql}').splitlines()
+        # should find column breaks in first 2-3l of fixed width result
+        for line in result[:3]:
+                # unity sql query column break row uses '-' char
+                if line[:1] == "-":
+                    separator_found = True
+                    # ..and column headers are separated by double spaces
+                    column_breaks = [cseparator.start() for cseparator in re.finditer('  ', line)]
+                    break
+
+        if not separator_found:
+            return {'fault': "unable to parse result, no separator line as expected"}  
+
+        # create slice map
+        slices = []
+        offset = 0
+        if not column_breaks:
+            # for single column results
+            slices.append(slice(offset,len(line)))
+        else:
+            # for multiple columns
+            slices = []
+            offset = 0
+            for column_break in column_breaks:
+                slices.append(slice(offset, column_break))
+                offset = column_break + 2
+            # add slice for the last column
+            slices.append(slice(offset,len(line)))
+        
+        # parse result into csv
+        result_csv = io.StringIO()
+        csv_writer = csv.writer(result_csv)
+        for line in result:
+            if line[:2] != "--":
+                csv_writer.writerow([line[slice].strip() for slice in slices])
+        return (result_csv.getvalue())
